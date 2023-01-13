4a8f1cc4e1dc4bbe9a6b7e6e6cf3057199f35d5e
shakiroye <oyeossiadewoye@gmail.com>
2023-01-13 07:44:59 +0000

    delete task and prop drilling

--- src/App.js
+++ src/App.js
@@ -17,10 +17,20 @@ function App() {
       reminder: true,
     },
   ]);
+
+  //Delete task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks Available"
+      )}
     </div>
   );
 }
