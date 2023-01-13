6f9e66c82ee8d06baf2ad08ff6f475b4003ecc35
shakiroye <oyeossiadewoye@gmail.com>
2023-01-13 13:42:41 +0000

    add task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -18,6 +20,14 @@ function App() {
     },
   ]);
 
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = { id, ...task };
+
+    setTasks([...tasks, newTask]);
+  };
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -34,7 +44,12 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log("click");
-      };
-
-  return <header className='header'>
-    <h1>{title}</h1>
-    <Button color='green' text='Add' onClick={onClick}/>
-  </header>;
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
 };
 
-Header.defaultProps={
-    title:'Task Tracker',
-}
+Header.defaultProps = {
+  title: "Task Tracker",
+};
 
-Header.propTypes={
-    title: PropTypes.string.isRequired,
-}
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 //CSS in JS
 // const headingStyle={
