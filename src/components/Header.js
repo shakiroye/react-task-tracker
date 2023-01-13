90f01ce2a71988d9ac9851c0f525f1ad147f1f91
shakiroye <oyeossiadewoye@gmail.com>
2023-01-13 06:18:12 +0000

    props and propTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+
+const Header = ({title}) => {
   return <header>
-    <h1>Task Tracker</h1>
+    <h1>{title}</h1>
   </header>;
 };
+
+Header.defaultProps={
+    title:'Task Tracker',
+}
+
+Header.propTypes={
+    title: PropTypes.string.isRequired,
+}
 
 export default Header;
