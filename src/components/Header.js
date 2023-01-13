b1cd6cd4efb5485d364157f0bda839eb2011d0e5
shakiroye <oyeossiadewoye@gmail.com>
2023-01-13 06:39:32 +0000

    styling and event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({title}) => {
-  return <header>
+    const onClick = () => {
+        console.log("click");
+      };
+
+  return <header className='header'>
     <h1>{title}</h1>
+    <Button color='green' text='Add' onClick={onClick}/>
   </header>;
 };
 
@@ -13,5 +19,11 @@ Header.defaultProps={
 Header.propTypes={
     title: PropTypes.string.isRequired,
 }
+
+//CSS in JS
+// const headingStyle={
+//     color:'red',
+//     backgroundColor:'black',
+// }
 
 export default Header;
